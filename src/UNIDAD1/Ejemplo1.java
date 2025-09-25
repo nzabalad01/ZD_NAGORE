3dbbb8e015cf3716b44440fe708c65995cb6d409
diurno <diurno@equipo-00>
2025-09-25 10:14:32 +0200

    Ejercicio1

---
+++ src/UNIDAD1/Ejemplo1.java
@@ -0,0 +1,29 @@
+package UNIDAD1;
+
+public class Ejemplo1 {
+		public static void main(String[] args) {
+		// Enteros
+		int edad = 20;
+		long poblacion = 47002200L;
+		int contador=0;
+		contador=9;
+		contador=contador-3;
+		// Decimales
+		float precio = 9.49f;
+		double pi = 3.1415926535;
+		// Carácter
+		char inicial = 'A';
+		// Booleano
+		boolean aprobado = true;
+		System.out.println("Edad: " + edad);
+		System.out.println("Población: " + poblacion);
+		System.out.println("Precio: " + precio);
+		System.out.println("PI: " + pi);
+		System.out.println("Inicial: " + inicial);
+		System.out.println("¿Aprobado? " + aprobado);
+		System.out.println("Contador: "+ contador);
+		
+	
+		}
+		
+}
