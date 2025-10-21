ba21580722be5de39c1fd92837f298364fc060f2
nzabalad01 <nzabalad01@educarex.es>
2025-10-21 10:36:47 +0200

    guau

--- src/UT02/Ejercicio_1_1.java
+++ src/UT02/Ejercicio_1_1.java
@@ -1,17 +1,12 @@
 package UT02;
 
 public class Ejercicio_1_1 {
-
-String nombre;
-String raza;
-
-public void ladrar () {
-
-	System.out.println(El nombre:" + nombre + "esta ladrando:");
-	System.out.println("guau guau");
-	System.out.println();
 	
+	String nombre;
+	String raza;
 	
-}
-
+	public void ladrar() {
+	System.out.println("Guau guau ");
+	
+	}
 }
