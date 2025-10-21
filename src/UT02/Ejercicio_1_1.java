dda2d2c745935abce817a46ab5aa4fa9e1045e07
nzabalad01 <nzabalad01@educarex.es>
2025-10-21 10:29:42 +0200

    rtert

---
+++ src/UT02/Ejercicio_1_1.java
@@ -0,0 +1,17 @@
+package UT02;
+
+public class Ejercicio_1_1 {
+
+String nombre;
+String raza;
+
+public void ladrar () {
+
+	System.out.println(El nombre:" + nombre + "esta ladrando:");
+	System.out.println("guau guau");
+	System.out.println();
+	
+	
+}
+
+}
--- src/UT02/Empleado.java
+++ src/UT02/Empleado.java
@@ -2,4 +2,13 @@ package UT02;
 
 public class Empleado {
 
+public static void main(String a[]) {
+	
+	String nombre="Nagore";
+	int edad=0;
+	
+	
+	
+	System.out.println("El nombre:" + nombre+"Edad es:" + edad);
+}
 }
