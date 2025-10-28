982cb722b3bd94c99bbda84ce2dbde71d259e354
nzabalad01 <nzabalad01@educarex.es>
2025-10-28 11:09:51 +0100

    Signo

---
+++ src/UT03/NumeroSigno.java
@@ -0,0 +1,28 @@
+package UT03;
+
+import java.until.Scanner;
+
+public class NumeroSigno {
+
+	public static void main(String[] args) {
+		java.util.Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Introduce número");
+		int número = teclado.nextInt();
+		
+		if (número>0) {
+
+			 System.out.println("El número introducido es positivo");
+			} else if (número = 0) {
+			 System.out.println("El número introducido es igual a 0");
+			} else if (número < 0) {
+			 System.out.println("El número introducido es negativo");
+			} 
+			 
+		}
+		
+	}
+		
+
+
+
