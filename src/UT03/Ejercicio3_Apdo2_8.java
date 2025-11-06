e7bfb1b78fa3a0bd295a14caecf2cfbed5c88cda
nzabalad01 <nzabalad01@educarex.es>
2025-11-06 11:06:53 +0100

    Ejercicio3apdo2_8

--- src/UT03/Ejercicio3_Apdo2_8.java
+++ src/UT03/Ejercicio3_Apdo2_8.java
@@ -8,12 +8,12 @@ public class Ejercicio3_Apdo2_8 {
 	public static void main(String[] args) {
 		int suma = 0;
 		Scanner teclado = new Scanner(System.in);
-		System.out.print("Introduce un número y calcularé la suma total."
-				+ "(para terminar Introduce un 0"): ");"
+		System.out.print("Introduce un número y calcularé la suma total " 
+				+ "(para terminar Introduce un 0): ");
 		int numero = teclado.nextInt();
 		while (numero!=0) {
 			suma= suma+numero;
-			nunero = teclado.nextInt();
+			numero = teclado.nextInt();
 		}
 		System.out.println("La suma de todos los números introducidos es:: " + suma);
 		teclado.close();
