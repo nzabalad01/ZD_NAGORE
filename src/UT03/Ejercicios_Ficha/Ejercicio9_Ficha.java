670f2e0280c9056afba7bd343e280b32f515cc2f
nzabalad01 <nzabalad01@educarex.es>
2025-11-17 11:07:31 +0100

    17/11/25

--- src/UT03/Ejercicios_Ficha/Ejercicio9_Ficha.java
+++ src/UT03/Ejercicios_Ficha/Ejercicio9_Ficha.java
@@ -1,5 +1,24 @@
 package UT03.Ejercicios_Ficha;
 
-public class Ejercicio9_Ficha {
+import java.util.Scanner;
 
+public class Ejercicio9_Ficha {
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Ingrese la primera nota: ");
+		double nota1 = entrada.nextDouble();
+		
+		System.out.print("Ingrese la primera nota: ");
+		double nota2 = entrada.nextDouble();
+		
+		System.out.print("Ingrese la primera nota: ");
+		double nota3 = entrada.nextDouble();
+		
+		double promedio = (nota1 + nota2 + nota3)/3;
+		if (promedio >=7) {
+			System.out.println("Promocio");
+		}
+	}
 }
