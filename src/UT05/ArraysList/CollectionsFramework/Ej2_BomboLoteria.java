df1dd81539cece207406dc97dce992ca149da9e0
nzabalad01 <nzabalad01@educarex.es>
2026-03-01 20:11:11 +0100

    01/03/2026

--- src/UT05/ArraysList/CollectionsFramework/Ej2_BomboLoteria.java
+++ src/UT05/ArraysList/CollectionsFramework/Ej2_BomboLoteria.java
@@ -1,12 +1,36 @@
 package UT05.ArraysList.CollectionsFramework;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * Queremos generar una combinación de la Lotería Primitiva
+ * (6 números entre 1 y 49).
+ * Genera números aleatorios y mételos en el set hasta que el tamaño 
+ * (size()) sea 6.
+ */
 public class Ej2_BomboLoteria {
 
-	/*Queremos generar una combinación de la Lotería Primitiva (6 números entre 1 y 49).
-	Usa un HashSet<Integer>.
-	Genera números aleatorios y mételos en el set hasta que el tamaño (size()) sea 6.
-	Reflexión: ¿Por qué usamos un while (set.size() < 6) y no un for de 6 vueltas? (Respuesta: porque si sale un repetido, el Set no crece, y necesitamos asegurar 6 distintos).*/
+	public static void main(String[] args) {
+
+		Set<Integer> bomboLoteria = new HashSet<>();
+		int ale=0;
+		while (bomboLoteria.size()<6) {
+			ale = (int)(Math.random() * (49))+1;
+			bomboLoteria.add(ale);
+		}
 		
-	
-	
+		System.out.println("La combinación ganadora de la Primitiva es la siguiente: ");
+		
+		//System.out.println(bomboLoteria);
+		
+		for (Integer numero : bomboLoteria) {
+			System.out.println(numero);
+		}
+		
+		
+
+		
+	}
+
 }
--- src/UT05/ArraysList/CollectionsFramework/Ej3_Diccionario.java
+++ src/UT05/ArraysList/CollectionsFramework/Ej3_Diccionario.java
@@ -1,12 +1,49 @@
 package UT05.ArraysList.CollectionsFramework;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/*
+ * Crea un pequeño traductor.
+ * Carga inicialmente en un HashMap 5 palabras 
+ * (ej: "Perro" -> "Dog", "Gato" -> "Cat").
+ * Pide una palabra en español al usuario.
+ * 		Si existe, muestra su traducción. 
+ * 		Si no existe, pregúntale al usuario cómo se dice en inglés, 
+ * 					añádela al mapa y dale las gracias por enseñarte.
+ */
 public class Ej3_Diccionario {
 
-	/*Crea un pequeño traductor.
-	Carga inicialmente en un HashMap 5 palabras (ej: "Perro" -> "Dog", "Gato" -> "Cat").
-	Pide una palabra en español al usuario.
-	Si existe, muestra su traducción. Si no existe, pregúntale al usuario cómo se dice en inglés, añádela al mapa y dale las gracias por enseñarte.*/
-	
-	
-	
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+		Map<String, String> diccionario = new HashMap<>();
+		
+		diccionario.put("Perro", "Dog");
+		diccionario.put("Gato", "Cat");
+		diccionario.put("Casa", "House");
+		diccionario.put("Coche", "Car");
+		diccionario.put("Pepino", "Cucumber");
+		
+		System.out.println("Bienvenido al Diccionario Spanish/English!!!");
+		System.out.println("Pregúntame qué palabra quieres traducir.");
+		System.out.println("(Fin para terminar.)");
+		String palabra="";
+		String traduccion="";
+		do {
+			System.out.println("Palabra en español: ");
+			palabra=entrada.nextLine();
+			if (diccionario.containsKey(palabra)) {
+				System.out.println("En inglés se dice: "+diccionario.get(palabra));
+			} else {
+				System.out.println("No sé traducir esa palabra. Dímelo tú:");
+				traduccion=entrada.nextLine();
+				diccionario.put(palabra, traduccion);
+				System.out.println("Gracias por enseñarme. ");
+			}
+		}while(!palabra.equalsIgnoreCase("fin"));
+		
+	}
+
 }
